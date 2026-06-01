2064da98b18be4f8caea45edef9ed24e61dd4c6a
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-06-01 20:38:54 +0530

    Updated functionality

--- Jokes.py
+++ Jokes.py
@@ -1,6 +1,10 @@
 import json as js
+import random as rnd
 
 def tellJoke():
     with open("jokes.json","r",encoding="utf-8") as jfile:
         jokes=js.load(jfile)
-        
+        joke=jokes["joke"]
+        joke=rnd.choice(joke)
+        print(joke["joke"])
+tellJoke()
