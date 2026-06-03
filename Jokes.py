f75c404051c340932b513da0eab43a3d5550f0f3
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-06-04 05:25:40 +0530

    Added everything needed worth the joke file

--- Jokes.py
+++ Jokes.py
@@ -4,7 +4,7 @@ import random as rnd
 def tellJoke():
     with open("jokes.json","r",encoding="utf-8") as jfile:
         jokes=js.load(jfile)
-        joke=jokes["joke"]
-        joke=rnd.choice(joke)
+        ind_joke=rnd.randint(0,len(jokes)-1)
+        joke=jokes[ind_joke]
         print(joke["joke"])
 tellJoke()
